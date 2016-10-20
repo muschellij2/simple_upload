c20dff41c44af80285ba97d484b8b107abef3791
muschellij2 <muschellij2@gmail.com>
2016-10-20 01:38:41 -0400

    updating simple upload

--- server.R
+++ server.R
@@ -3,6 +3,13 @@ library(shiny)
 library(shinyapps)
 ### make shiny max request size 30MB
 options(shiny.maxRequestSize=30*1024^2)
+tdir = tempfile()
+repo = git2r::clone(url = "https://github.com/muschellij2/kelly_uploads",
+             local_path = tdir)
+
+pat = readLines("kelly_pat.txt")
+Sys.setenv("KELLY_PAT" = pat)
+cred = cred_token("KELLY_PAT")
 
 shinyServer(function(input, output, session) {
   # output$filetable <- renderTable({
@@ -11,21 +18,33 @@ shinyServer(function(input, output, session) {
         # User has not uploaded a file yet
      
         files = input$files
-        fnames = files$name
-        
-        types = files$type
+        # fnames = files$name
+        files$name = as.character(files$name)
+        files$datapath = as.character(files$datapath)
+        infiles = files$datapath
+        outfiles = file.path(tdir, files$name)
         print(files)
-        
-        
+        print(infiles)
+        print(outfiles)
+        file.copy(from = files$datapath,
+                  to = outfiles, overwrite = TRUE)
+        print(files)
+        git2r::add(repo, path = outfiles)
+        mess = paste0("Uploading ", paste(files$name, collapse = ", "),
+                      "files")
+        git2r::commit(repo = repo, message = mess)
+        git2r::push(repo, name = "origin", "refs/heads/gh-pages", 
+                    credentials = cred)
+
     } else {
       
     }
-      return(list(nii=nii, filename=outfile))
+      return("hey")
   })
 
 
-  output$out_info <- renderText({
+  output$out_info <- renderUI({
     res = upload_file()
-    
+    return(res)
   })
 })
--- ui.R
+++ ui.R
@@ -1,24 +1,13 @@
-library(shinyIncubator)
 shinyUI(pageWithSidebar(
-  headerPanel("Online DICOM TO NIfTI Converter"),
+  headerPanel("Simple Upload to GitHub"),
   sidebarPanel(
-    fileInput("files", "File data", multiple=TRUE, 
-              accept = c("application/dicom", 
-                         "application/zip",
-                         "application/x-gzip")),
-#     uiOutput("sld_window"),
-    selectInput("dcmniicmd", "Which dcm2nii?", 
-                choices =c("dcm2niix", "dcm2nii"),
-                selected = "dcm2niix"),
-    checkboxInput("viewimg", "View img?", TRUE),
-    checkboxInput("gzipped", "gzip nii?", TRUE),
-    textInput("niifname", "Filename of nii (no .nii on it)"),
-    downloadButton('dlimg', 'Download NIfTI')
+    fileInput("files", "Files to upload", multiple = TRUE)
   ),
   mainPanel(
-#     progressInit(),
-    plotOutput("ortho"),
-    p("Code on ", 
-      a("GitHub", href="https://github.com/muschellij2/dcm2nii_shiny"))
+    h3("Current files uploaded, all hyperlinked"),
+    htmlOutput("curr_files"),
+    h3("Files uploaded, all hyperlinked"),
+    htmlOutput("out_info")
+    
   )
 ))
